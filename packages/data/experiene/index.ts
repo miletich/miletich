4648b1f1b67b5fbaa3756e9cffd1b750c941059d
Dušan Miletić <d@miletich.cc>
2023-11-20 20:50:11 +0100

    feat(data): add experience schema

---
+++ packages/data/experiene/index.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+
+export const datePointSchema = z.object({
+  from: z.number(),
+  to: z.number(),
+});
+export type DatePoint = z.infer<typeof datePointSchema>;
+
+export const positionSchema = z.object({
+  id: z.number(),
+  position: z.string(),
+  from: datePointSchema,
+  to: datePointSchema,
+  description: z.array(z.string()),
+  tech: z.array(z.string()),
+});
+export type Position = z.infer<typeof positionSchema>;
+
+export const experienceSchema = z.array(positionSchema);
