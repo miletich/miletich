1276da6e180ea496b27044032ae1c8c6a167a19a
Dušan Miletić <d@miletich.cc>
2023-11-21 21:12:36 +0100

    feat(common): parametrize locale in formatDate

--- packages/common/utils/format-date.ts
+++ packages/common/utils/format-date.ts
@@ -1,11 +1,10 @@
 import type { DatePoint } from 'data/experience';
-import { locale } from '../../../apps/web/app/consts';
 
 const options = {
   year: 'numeric',
   month: 'short',
 } as const;
 
-type FormatDate = (date: DatePoint) => string;
-export const formatDate: FormatDate = ({ year, month }) =>
+type FormatDate = (date: DatePoint, locale?: string) => string;
+export const formatDate: FormatDate = ({ year, month }, locale = 'en-GB') =>
   new Date(`${year}-${month}-01`).toLocaleDateString(locale, options);
