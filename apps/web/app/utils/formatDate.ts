1884e32081cf37cf5855692a954fa6a62455dcba
Dušan Miletić <d@miletich.cc>
2023-11-21 15:43:48 +0100

    feat(web): formatDate

---
+++ apps/web/app/utils/formatDate.ts
@@ -0,0 +1,11 @@
+import type { DatePoint } from 'data/experience';
+import { locale } from '../consts';
+
+const options = {
+  year: 'numeric',
+  month: 'short',
+} as const;
+
+type FormatDate = (date: DatePoint) => string;
+export const formatDate: FormatDate = ({ year, month }) =>
+  new Date(`${year}-${month}-01`).toLocaleDateString(locale, options);
