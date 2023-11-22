a752a6d17ec121412106473574b58dc640ad3e0a
Dušan Miletić <d@miletich.cc>
2023-11-22 19:20:08 +0100

    feat(common): factor out consts from Logo

---
+++ packages/common/Logo/consts.ts
@@ -0,0 +1,30 @@
+export const m1 = `
+    M 2,0
+    L 3,0
+    L 1,2
+    L 0,2
+    L 2,0z
+  `;
+export const m2 = `
+    M 4,0
+    L 5,0
+    L 3,2
+    L 2,2
+    L 4,0z
+  `;
+export const m3 = `
+    M 5,1
+    L 5,2
+    L 4,2
+    L 5,1z
+  `;
+export const d = `
+    M 5,0
+    L 6,0
+    L 7,1
+    L 6,2
+    L 5,2
+    L 6,1,
+    L 5,0z
+  `;
+export const viewBox = '0 0 7 2';
