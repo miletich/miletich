008354e8d9ac1e17b87cbcd94daaeac95ab00ef2
Dušan Miletić <d@miletich.cc>
2023-11-22 21:19:01 +0100

    refactor(pdf): Li -> P

--- apps/pdf/app/cv/components/P.tsx
+++ apps/pdf/app/cv/components/P.tsx
@@ -2,6 +2,6 @@ import { Text } from '@react-pdf/renderer';
 import type { PropsWithChildren } from 'react';
 import { styles } from '../styles';
 
-export default function Li({ children }: PropsWithChildren): JSX.Element {
-  return <Text style={styles.li}>{children}</Text>;
+export default function P({ children }: PropsWithChildren): JSX.Element {
+  return <Text style={styles.p}>{children}</Text>;
 }
